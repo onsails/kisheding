4054fe17c01ca7e6ff2307f7b8fb2ffeead7430d
Andrey Kuznetsov <fear@loathing.in>
2017-09-16 14:59:11 +0300

    congrats

--- scripts/dance.js
+++ scripts/dance.js
@@ -1,23 +1,31 @@
-
 var v = document.getElementById('composition');
-v.play();
+
+setTimeout(function() {
+  v.play();
+  document.getElementById('start').style.display = 'none';
+  v.style.display = '';
+}, 3000);
 
 var handlers = {
   1: function(){
     if (v.currentTime >= 12) {
       var text = document.getElementById('love-song');
+      var flowers = document.getElementById('flowers');
       v.pause();
       v.style.display = 'none';
       text.style.display = '';
+      flowers.style.display = '';
       setTimeout(function() {
         text.style.display = 'none';
+        flowers.style.display = 'none';
         v.currentTime = 12.85;
         v.style.display = '';
         v.play();
-        v.ontimeupdate = handlers[2];
-      }, 5000);
+        v.ontimeupdate = handlers[3];
+      }, 4000);
     }
   },
+  // not used
   2: function() {
     if (v.currentTime >= 66.6) {
       var text = document.getElementById('brush');
@@ -45,22 +53,30 @@ var handlers = {
         v.style.display = '';
         v.play();
         v.ontimeupdate = handlers[4];
-      }, 5000);
+      }, 3000);
     }
   },
   4: function () {
     if (v.currentTime >= 164) {
-      var text = document.getElementById('fasion');
+      var text = document.getElementById('guests');
       v.pause()
       v.style.display = 'none';
       text.style.display = '';
       setTimeout(function() {
         text.style.display = 'none';
-        v.currentTime = 176.9;
+        v.currentTime = 177;
         v.style.display = '';
         v.play();
-        v.ontimeupdate = null;
-      }, 5000);
+        v.ontimeupdate = handlers[5];
+      }, 3000);
+    }
+  },
+  5: function () {
+    if (v.currentTime >= 242) {
+      var text = document.getElementById('gift');
+      v.pause()
+      v.style.display = 'none';
+      text.style.display = '';
     }
   },
 }
